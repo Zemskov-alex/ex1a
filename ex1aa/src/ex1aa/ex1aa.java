a6414191a9a64c3923728e5526c092e6c905ea2e
test <test@ws01>
2020-06-01 19:22:49 +0300

    Second

---
+++ ex1aa/src/ex1aa/ex1aa.java
@@ -0,0 +1,26 @@
+package ex1aa;
+
+public class ex1aa {
+	
+		 public static void main(String[] args) {
+		        int i = 3;
+		        switch(i) {
+		            default:
+		                System.out.println("default");
+		//break;
+		            case 1:
+		                System.out.println("1");
+		//break;
+
+		            case 2:
+		                System.out.println("2");
+		//break;
+
+		        }
+		    }
+		}
+
+
+	
+
+
